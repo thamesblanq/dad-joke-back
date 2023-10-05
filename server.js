24a7817c329d9d2c884dd99ac2a94a3e885e68b1
blanq31 <obiwulugodswill@gmail.com>
2023-10-05 04:21:14 +0100

    route created

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+//middlewares
 app.use(express.json());
-app.use(express.urlencoded())
+
+//routes
+app.get('/joke', (req, res) => {
+    try{
+        res.status(200).json({ "message": process.env.REACT_APP_API_KEY });
+    } catch(err) {
+        res.status(500).json({ "message": err.message });
+    }
+    
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`)
