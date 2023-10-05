b28c4346f8032108c9a340a444fdda7d539ffb0e
blanq31 <obiwulugodswill@gmail.com>
2023-10-05 15:08:59 +0100

    further optimizations

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const axios = require('axios');
 const PORT = process.env.PORT;
 
 
@@ -10,9 +11,18 @@ app.use(cors());
 app.use(express.json());
 
 //routes
-app.get('/joke', (req, res) => {
+app.get('/joke', async (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://dad-jokes.p.rapidapi.com/random/joke',
+        headers: {
+          'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+          'X-RapidAPI-Host': process.env.REACT_APP_HOST
+        }
+      };
     try{
-        res.status(200).json({ "message": process.env.REACT_APP_API_KEY });
+        const response = await axios.request(options);
+        res.status(200).json({ "message": response.data.body[0]})
     } catch(err) {
         res.status(500).json({ "message": err.message });
     }
